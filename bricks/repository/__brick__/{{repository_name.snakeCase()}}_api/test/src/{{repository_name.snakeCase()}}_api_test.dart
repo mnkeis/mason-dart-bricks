ce2f9a23526931ac5977b18c1789ebe4d3b6e426
Mauro Casse <mnkeis@gmail.com>
2022-08-20 21:47:11 -0300

    Updated tests

--- bricks/repository/__brick__/{{repository_name.snakeCase()}}_api/test/src/{{repository_name.snakeCase()}}_api_test.dart
+++ bricks/repository/__brick__/{{repository_name.snakeCase()}}_api/test/src/{{repository_name.snakeCase()}}_api_test.dart
@@ -1,23 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:{{repository_name.snakeCase()}}_api/{{repository_name.snakeCase()}}_api.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-class Test{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api implements {{#pascalCase}}{{repository_name}}{{/pascalCase}}Api {
-  Test{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api();
-
-  {{#methods}}
-  @override
-  {{#hasSpecial}}{{{type}}}{{/hasSpecial}}{{^hasSpecial}}{{type}}{{/hasSpecial}} {{name.camelCase()}}({{#hasParameters}}{ {{#parameters}}
-    {{^isNullable}}required {{/isNullable}}{{#hasSpecial}}{{{type}}}{{/hasSpecial}}{{^hasSpecial}}{{type}}{{/hasSpecial}} {{name.camelCase()}},{{/parameters}}
-  }{{/hasParameters}}){
-    throw UnimplementedError();
-  }{{/methods}}
-} 
+class Mock{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api extends Mock implements {{#pascalCase}}{{repository_name}}{{/pascalCase}}Api {} 
 
 void main() {
   group('Test{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api', () {
     test('can be instantiated', () {
-      expect(Test{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api(), isNotNull);
+      expect(Mock{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api(), isNotNull);
     });
   });
 }
--- bricks/repository/__brick__/{{repository_name.snakeCase()}}_repository/test/src/{{repository_name.snakeCase()}}_repository_test.dart
+++ bricks/repository/__brick__/{{repository_name.snakeCase()}}_repository/test/src/{{repository_name.snakeCase()}}_repository_test.dart
@@ -1,22 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:{{repository_name.snakeCase()}}_api/{{repository_name.snakeCase()}}_api.dart';
 import 'package:{{repository_name.snakeCase()}}_repository/{{repository_name.snakeCase()}}_repository.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-class Test{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api implements {{#pascalCase}}{{repository_name}}{{/pascalCase}}Api {
-  Test{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api();
-
-  {{#methods}}
-  @override
-  {{#hasSpecial}}{{{type}}}{{/hasSpecial}}{{^hasSpecial}}{{type}}{{/hasSpecial}} {{name.camelCase()}}({{#hasParameters}}{ {{#parameters}}
-    {{^isNullable}}required {{/isNullable}}{{#hasSpecial}}{{{type}}}{{/hasSpecial}}{{^hasSpecial}}{{type}}{{/hasSpecial}} {{name.camelCase()}},{{/parameters}}
-  }{{/hasParameters}}){
-    throw UnimplementedError();
-  }{{/methods}}
-} 
+class Mock{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api extends Mock implements {{#pascalCase}}{{repository_name}}{{/pascalCase}}Api {} 
 
 void main() {
-  final test{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api = Test{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api();
+  final test{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api = Mock{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api();
   group('{{#pascalCase}}{{repository_name}}{{/pascalCase}}Repository', () {
     test('can be instantiated', () {
       expect({{#pascalCase}}{{repository_name}}{{/pascalCase}}Repository(test{{#pascalCase}}{{repository_name}}{{/pascalCase}}Api), isNotNull);
